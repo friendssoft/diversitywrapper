8640d694635107022f36b72ff89e4b279498f6b7
friendssoft <anwarshaik84@gmail.com>
2022-05-28 20:04:26 +0530

    DiversityWrapper changes

---
+++ src/test/java/com/wf/diversityWrapper/DiversityWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.diversityWrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiversityWrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
